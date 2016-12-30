602cdb53e879b72ca3973276f701584d1e8fc984
Matt-Hurd <sabahbob@gmail.com>
2016-12-30 11:50:30 -0800

    we good?

--- src/conversions/ft_conv_unimp.c
+++ src/conversions/ft_conv_unimp.c
@@ -6,7 +6,7 @@
 /*   By: mhurd <mhurd@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/26 19:17:17 by mhurd             #+#    #+#             */
-/*   Updated: 2016/12/01 18:57:46 by mhurd            ###   ########.fr       */
+/*   Updated: 2016/12/30 11:50:23 by mhurd            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,4 @@ void	ft_conv_unimp(char in, t_output *out, t_arg *flags, va_list *ap)
 	(void)out;
 	(void)in;
 	(void)flags;
-	out->str = ft_strnjoin(out->str, out->len, "unimplemented", 13);
-	out->len += 13;
 }
